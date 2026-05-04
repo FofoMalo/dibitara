4dffc8fedefea47b218915b2223925891c55d532
Fofomalo <maloflorent3@gmail.com>
2026-05-05 00:51:37 +0200

    Supprime définitivement les artefacts de build du dépôt

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_dibitara_app_DibitaraApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.dibitara.app.DibitaraApp"
-)
-public final class _com_dibitara_app_DibitaraApp {
-}
